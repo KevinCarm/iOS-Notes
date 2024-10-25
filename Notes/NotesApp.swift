ff516facde7a09c36a3a619270f4635557d04c28
Kevin CS <kevincarm016@hotmail.com>
2024-10-24 22:06:17 -0600

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Kevin Carmona.S on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
